cd428a3e3c2b536487c71779c49630066da3cd12
Rahul <rahulnallappa2016@u.northwestern.edu>
2019-12-26 20:35:09 -0500

    converted search with hooks

--- app/scripts/components/search.js
+++ app/scripts/components/search.js
@@ -1,47 +1,41 @@
-import React, { Component } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 const TMDBLogo = "./images/tmdb.svg";
 
-class SearchBox extends Component {
-  handleChange(event) {
-    event.target.select();
-  }
-  render() {
-    return (
-      <div className="col-xs-12 search-container nopadding">
-      
-      {/* ROW 1: wraps entire search component*/}
-        <div className="row">
+function SearchBox() {
 
-        {/* COL 1: displays logo */}
-          <div className="col-xs-12 col-sm-6 col-lg-5">
-            <a
-              href="./"
-              title="ReactJS TMDb Movie Search"
-              onclick="ga('send', 'event', 'link', 'internal', 'TMDB logo')"
-            >
-              <img src={TMDBLogo} className="logo" alt="The Movie Database" />
-            </a>
-          </div>
+  return (
+    <div className="col-xs-12 search-container nopadding">
+    
+    {/* ROW 1: wraps entire search component*/}
+      <div className="row">
 
-          {/* COL 2: displays search component */}
-          <div className="col-xs-12 col-sm-6 col-lg-7">
-            <form className="searchbox">
-              {/* <label> */}
-              <input
-                ref="search suggestion"
-                onClick={this.handleChange}
-                className="searchbox__input typeahead form-control"
-                type="text"
-                placeholder="Search Movie Title..."
-                id="q"
-              />
-              {/* </label> */}
-            </form>
-          </div>
+      {/* COL 1: displays logo */}
+        <div className="col-xs-12 col-sm-6 col-lg-5">
+          <a
+            href="./"
+            title="ReactJS TMDb Movie Search"
+            onclick="ga('send', 'event', 'link', 'internal', 'TMDB logo')"
+          >
+            <img src={TMDBLogo} className="logo" alt="The Movie Database" />
+          </a>
+        </div>
+
+        {/* COL 2: displays search component */}
+        <div className="col-xs-12 col-sm-6 col-lg-7">
+          <form className="searchbox">
+            {/* <label> */}
+            <input
+              className="searchbox__input typeahead form-control"
+              type="text"
+              placeholder="Search Movie Title..."
+              id="q"
+            />
+            {/* </label> */}
+          </form>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 module.exports = SearchBox;
